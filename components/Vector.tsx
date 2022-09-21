a40aa9eb2491acf1be45df28e24d3e5cfeb77bab
Michael Warner <michaelwarner@Michaels-MacBook-Pro.local>
2022-09-21 12:01:38 -0400

    optimize (mainly, vectors are now mutated instead of recreated)

--- components/Vector.tsx
+++ components/Vector.tsx
@@ -2,94 +2,256 @@ import * as THREE from 'three';
 import { type CartesianComponents } from '../helpers/store';
 import { font } from '../helpers/font';
 import { Material } from 'three';
+import { memo, useEffect, useRef, useState } from 'react';
 
 interface Props {
-  components: CartesianComponents;
+  x: number;
+  y: number;
+  z: number;
   color?: string;
   label?: string;
   opacity?: number;
   showComponentVectors?: boolean;
+  hide?: boolean;
 }
 
 const origin = new THREE.Vector3(0, 0, 0);
 
-const Vector = ({
-  components,
-  color,
-  label,
-  opacity,
-  showComponentVectors,
-}: Props) => {
-  const vector = new THREE.Vector3(...components);
-  const dir = vector.clone().normalize();
-  const length = vector.length();
+const Vector = memo(
+  ({ x, y, z, color, label, opacity, showComponentVectors, hide }: Props) => {
+    const [vectorsAreReady, setVectorsAreReady] = useState(false);
 
-  const arrow = new THREE.ArrowHelper(dir, origin, length, color, 0.2, 0.1);
+    // define refs for main vector
+    const vector = useRef<THREE.Vector3>();
+    const dir = useRef<THREE.Vector3>();
+    const arrow = useRef<THREE.ArrowHelper>();
 
-  if (opacity) {
-    (arrow.line.material as Material).transparent = true;
-    (arrow.line.material as Material).opacity = opacity;
-    (arrow.cone.material as Material).transparent = true;
-    (arrow.cone.material as Material).opacity = opacity;
-  }
+    // define refs for component-vector perpendicular to boost (x-axis)
+    const vectorClonePerp = useRef<THREE.Vector3>();
+    const perpCompProjectee = useRef<THREE.Vector3>();
+    const perpCompVec = useRef<THREE.Vector3>();
+    const perpCompDir = useRef<THREE.Vector3>();
+    const perpCompArrow = useRef<THREE.ArrowHelper>();
 
-  const perpCompVec =
-    showComponentVectors && new THREE.Vector3(0, components[1], components[2]);
-  const parCompVec =
-    showComponentVectors && new THREE.Vector3(components[0], 0, 0);
+    // define component-vector parallel to boost (x-axis) with refs
+    const vectorClonePar = useRef<THREE.Vector3>();
+    const parCompProjectee = useRef<THREE.Vector3>();
+    const parCompVec = useRef<THREE.Vector3>();
+    const parCompDir = useRef<THREE.Vector3>();
+    const parCompArrow = useRef<THREE.ArrowHelper>();
 
-  return (
-    <>
-      {perpCompVec &&
-        parCompVec &&
-        [perpCompVec, parCompVec].map((v, i) => {
-          const compVector = vector.clone().projectOnVector(v);
-          const compVectorDir = compVector.clone().normalize();
-          const compVectorLength = compVector.length();
-          const compVectorOrigin = v === perpCompVec ? origin : perpCompVec;
+    // (is this the right hook to use? I'm wondering about fast changes)
+    useEffect(() => {
+      // main vector
 
-          const compArrow = new THREE.ArrowHelper(
-            compVectorDir,
-            compVectorOrigin,
-            compVectorLength,
+      if (vector.current) {
+        vector.current.set(x, y, z);
+      } else {
+        vector.current = new THREE.Vector3(x, y, z);
+      }
+
+      if (dir.current) {
+        dir.current.set(x, y, z).normalize();
+      } else {
+        dir.current = vector.current.clone().normalize();
+      }
+
+      if (arrow.current) {
+        arrow.current.setDirection(dir.current);
+        arrow.current.setLength(vector.current.length(), 0.2, 0.1);
+      } else {
+        arrow.current = (() => {
+          const arrowHelper = new THREE.ArrowHelper(
+            dir.current,
+            origin,
+            vector.current.length(),
             color,
             0.2,
             0.1
           );
-          (compArrow.line.material as Material).transparent = true;
-          (compArrow.line.material as Material).opacity = 0.2;
-          (compArrow.cone.material as Material).transparent = true;
-          (compArrow.cone.material as Material).opacity = 0.2;
-          return <primitive key={i} object={compArrow} />;
-        })}
-      <primitive object={arrow} />
-      {label && (
-        <mesh
-          position={
-            components.map((component) =>
-              component < 0 ? component - 0.05 : component + 0.05
-            ) as CartesianComponents
+          if (opacity) {
+            (arrowHelper.line.material as Material).transparent = true;
+            (arrowHelper.line.material as Material).opacity = opacity;
+            (arrowHelper.cone.material as Material).transparent = true;
+            (arrowHelper.cone.material as Material).opacity = opacity;
           }
-        >
-          <textGeometry
-            args={[
-              label,
-              {
-                // @ts-ignore
-                font,
-                size: 0.4,
-                height: 0,
-              },
-            ]}
-          />
-          <meshLambertMaterial
-            color={color}
-            {...(opacity ? { transparent: true, opacity } : {})}
-          />
+          return arrowHelper;
+        })();
+      }
+
+      // component-vector perpendicular to boost (x-axis)
+
+      if (vectorClonePerp.current) {
+        vectorClonePerp.current.set(x, y, z);
+      } else {
+        vectorClonePerp.current = vector.current.clone();
+      }
+
+      if (perpCompProjectee.current) {
+        perpCompProjectee.current.set(0, y, z);
+      } else {
+        perpCompProjectee.current = new THREE.Vector3(0, y, z);
+      }
+
+      vectorClonePerp.current.projectOnVector(perpCompProjectee.current);
+
+      if (perpCompVec.current) {
+        perpCompVec.current.set(
+          vectorClonePerp.current.x,
+          vectorClonePerp.current.y,
+          vectorClonePerp.current.z
+        );
+      } else {
+        perpCompVec.current = vectorClonePerp.current.projectOnVector(
+          perpCompProjectee.current
+        );
+      }
+
+      if (perpCompDir.current) {
+        perpCompDir.current
+          .set(
+            vectorClonePerp.current.x,
+            vectorClonePerp.current.y,
+            vectorClonePerp.current.z
+          )
+          .normalize();
+      } else {
+        perpCompDir.current = perpCompVec.current.clone().normalize();
+      }
+
+      if (perpCompArrow.current) {
+        perpCompArrow.current.setDirection(perpCompDir.current);
+        perpCompArrow.current.setLength(perpCompVec.current.length(), 0.2, 0.1);
+      } else {
+        perpCompArrow.current = (() => {
+          const arrowHelper = new THREE.ArrowHelper(
+            perpCompDir.current,
+            origin,
+            perpCompVec.current.length(),
+            color,
+            0.2,
+            0.1
+          );
+          (arrowHelper.line.material as Material).transparent = true;
+          (arrowHelper.line.material as Material).opacity = 0.2;
+          (arrowHelper.cone.material as Material).transparent = true;
+          (arrowHelper.cone.material as Material).opacity = 0.2;
+          return arrowHelper;
+        })();
+      }
+
+      // component-vector parallel to boost (x-axis)
+
+      if (vectorClonePar.current) {
+        vectorClonePar.current.set(x, y, z);
+      } else {
+        vectorClonePar.current = vector.current.clone();
+      }
+
+      if (parCompProjectee.current) {
+        parCompProjectee.current.set(x, 0, 0);
+      } else {
+        parCompProjectee.current = new THREE.Vector3(x, 0, 0);
+      }
+
+      vectorClonePar.current.projectOnVector(parCompProjectee.current);
+
+      if (parCompVec.current) {
+        parCompVec.current.set(
+          vectorClonePar.current.x,
+          vectorClonePar.current.y,
+          vectorClonePar.current.z
+        );
+      } else {
+        parCompVec.current = vectorClonePar.current.projectOnVector(
+          parCompProjectee.current
+        );
+      }
+
+      if (parCompDir.current) {
+        parCompDir.current
+          .set(
+            vectorClonePar.current.x,
+            vectorClonePar.current.y,
+            vectorClonePar.current.z
+          )
+          .normalize();
+      } else {
+        parCompDir.current = parCompVec.current.clone().normalize();
+      }
+
+      if (parCompArrow.current) {
+        parCompArrow.current.setDirection(parCompDir.current);
+        parCompArrow.current.setLength(parCompVec.current.length(), 0.2, 0.1);
+        parCompArrow.current.position.set(
+          perpCompVec.current.x,
+          perpCompVec.current.y,
+          perpCompVec.current.z
+        );
+      } else {
+        parCompArrow.current = (() => {
+          const arrowHelper = new THREE.ArrowHelper(
+            parCompDir.current,
+            perpCompVec.current,
+            parCompVec.current.length(),
+            color,
+            0.2,
+            0.1
+          );
+          (arrowHelper.line.material as Material).transparent = true;
+          (arrowHelper.line.material as Material).opacity = 0.2;
+          (arrowHelper.cone.material as Material).transparent = true;
+          (arrowHelper.cone.material as Material).opacity = 0.2;
+          return arrowHelper;
+        })();
+      }
+
+      setVectorsAreReady(true);
+    }, [color, opacity, x, y, z]);
+
+    return vectorsAreReady ? (
+      <>
+        <mesh visible={!hide && showComponentVectors}>
+          <primitive object={perpCompArrow.current} />
+          <primitive object={parCompArrow.current} />
         </mesh>
-      )}
-    </>
-  );
-};
+        <mesh visible={!hide}>
+          <primitive object={arrow.current} />
+        </mesh>
+        {/* label stuff is getting recreated every render at the moment; would be better not to */}
+        {label && (
+          <mesh
+            position={
+              [x, y, z].map((component) =>
+                component < 0 ? component - 0.05 : component + 0.05
+              ) as CartesianComponents
+            }
+            visible={!hide}
+          >
+            <textGeometry
+              args={[
+                label,
+                {
+                  // @ts-ignore
+                  font,
+                  size: 0.4,
+                  height: 0,
+                },
+              ]}
+            />
+            <meshLambertMaterial
+              color={color}
+              {...(opacity ? { transparent: true, opacity } : {})}
+            />
+          </mesh>
+        )}
+      </>
+    ) : (
+      <></>
+    );
+  }
+);
+
+Vector.displayName = 'Vector';
 
 export default Vector;
--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,9 @@ const dot = (
 
 const round = (n: number) => parseFloat(n.toFixed(12));
 
+const boostVelocity = new THREE.Vector3();
+const boostUnitVelocity = boostVelocity.clone();
+
 enum Color {
   E = 'blue',
   B = 'crimson',
@@ -44,6 +47,8 @@ const textColor: { [C in Color]: `text-[${C}]` } = /* tw */ {
   darkslategray: 'text-[darkslategray]',
 };
 
+const axes = <Axes />;
+
 const CameraController = forwardRef<OrbitControls | undefined>((_, ref) => {
   const { camera, gl } = useThree();
   useEffect(() => {
@@ -58,6 +63,60 @@ const CameraController = forwardRef<OrbitControls | undefined>((_, ref) => {
   return null;
 });
 CameraController.displayName = 'CameraController';
+
+const instructions = (
+  <div className="space-y-10">
+    <h1 className="text-2xl sm:text-3xl">
+      Lorentz Transformation of the Electric and Magnetic Fields, Visualized
+    </h1>
+    <details>
+      <summary>Instructions</summary>
+      <div className="max-w-prose space-y-3 p-4">
+        <p>
+          This visualization demonstrates how the electric and magnetic fields
+          transform under a Lorentz boost.
+        </p>
+        <p>
+          Use the inputs below the visualization to set the Cartesian components
+          of the electric- and magnetic-field vectors in the original "unprimed"
+          frame, as well as (the x-component of) the boost-velocity. The
+          electric- and magnetic-field vectors in the "primed" frame are
+          calculated and rendered automatically.
+        </p>
+        <p>
+          The inputs can only accept number-values, even while you're typing the
+          number. Since a minus-sign by itself isn't interpreted as a number,
+          entering a negative number is a bit cumbersome: you have to type at
+          least one digit <em>before</em> inserting the minus sign at the start.
+          Sorry about that.
+        </p>
+        <p>
+          Electric and magnetic fields are measured in the same unit. The
+          boost-speed is normalized (i.e., it's given as a fraction of the speed
+          of light and must be less than 1). Finally, the Cartesian axes and
+          their labels are fixed; perhaps in the future I'll make them
+          adjustable or have them adapt to the vectors in the space.
+        </p>
+        <p>Here is how the "camera" works:</p>
+        <ul className="ml-4 list-disc">
+          <li>
+            To orbit, use the left mouse-button (or one-finger move for touch).
+          </li>
+          <li>
+            To zoom, use the mousewheel or the middle mouse-button (or
+            two-finger spread/squish for touch).
+          </li>
+          <li>
+            To pan, use the right mouse-button (or two-finger move for touch).
+            Panning will change the focal point for orbiting and zooming, but
+            you can restore it with the "Reset Camera" button below the
+            visualization.
+          </li>
+        </ul>
+      </div>
+    </details>
+  </div>
+);
 
 const Page = () => {
   const cameraRef = useRef<OrbitControls>();
@@ -84,8 +143,9 @@ const Page = () => {
   const [showComponentVectors, setShowComponentVectors] = useState(false);
   const [showPoynting, setShowPoynting] = useState(false);
 
-  const boostVelocity = new THREE.Vector3(boostVelocityX, 0, 0);
-  const boostUnitVelocity = boostVelocity.clone().normalize();
+  boostVelocity.set(boostVelocityX, 0, 0);
+  boostUnitVelocity.set(boostVelocityX, 0, 0).normalize();
+
   const boostUnit = [
     boostUnitVelocity.x,
     boostUnitVelocity.y,
@@ -119,108 +179,62 @@ const Page = () => {
         </title>
       </Head>
       <main className="container mt-10 flex h-screen w-screen flex-col space-y-10">
-        <div className="space-y-10">
-          <h1 className="text-2xl sm:text-3xl">
-            Lorentz Transformation of the Electric and Magnetic Fields,
-            Visualized
-          </h1>
-          <details>
-            <summary>Instructions</summary>
-            <div className="max-w-prose space-y-3 p-4">
-              <p>
-                This visualization demonstrates how the electric and magnetic
-                fields transform under a Lorentz boost.
-              </p>
-              <p>
-                Use the inputs below the visualization to set the Cartesian
-                components of the electric- and magnetic-field vectors in the
-                original "unprimed" frame, as well as (the x-component of) the
-                boost-velocity. The electric- and magnetic-field vectors in the
-                "primed" frame are calculated and rendered automatically.
-              </p>
-              <p>
-                The inputs can only accept number-values, even while you're
-                typing the number. Since a minus-sign by itself isn't
-                interpreted as a number, entering a negative number is a bit
-                cumbersome: you have to type at least one digit <em>before</em>{' '}
-                inserting the minus sign at the start. Sorry about that.
-              </p>
-              <p>
-                Electric and magnetic fields are measured in the same unit. The
-                boost-speed is normalized (i.e., it's given as a fraction of the
-                speed of light and must be less than 1). Finally, the Cartesian
-                axes and their labels are fixed; perhaps in the future I'll make
-                them adjustable or have them adapt to the vectors in the space.
-              </p>
-              <p>Here is how the "camera" works:</p>
-              <ul className="ml-4 list-disc">
-                <li>
-                  To orbit, use the left mouse-button (or one-finger move for
-                  touch).
-                </li>
-                <li>
-                  To zoom, use the mousewheel or the middle mouse-button (or
-                  two-finger spread/squish for touch).
-                </li>
-                <li>
-                  To pan, use the right mouse-button (or two-finger move for
-                  touch). Panning will change the focal point for orbiting and
-                  zooming, but you can restore it with the "Reset Camera" button
-                  below the visualization.
-                </li>
-              </ul>
-            </div>
-          </details>
-        </div>
+        {instructions}
         <Canvas className="max-h-screen min-h-[600px] flex-1 px-6 [&>*]:border">
           <CameraController ref={cameraRef} />
           <ambientLight />
-          <Axes />
+          {axes}
           <Vector
-            components={eField}
+            x={eField[0]}
+            y={eField[1]}
+            z={eField[2]}
             color={Color.E}
             label="E"
             showComponentVectors={showComponentVectors}
           />
           <Vector
-            components={bField}
+            x={bField[0]}
+            y={bField[1]}
+            z={bField[2]}
             color={Color.B}
             label="B"
             showComponentVectors={showComponentVectors}
           />
+          <Vector x={boostVelocityX} y={0} z={0} color={Color.V} label="v" />
           <Vector
-            components={[boostVelocityX, 0, 0]}
-            color={Color.V}
-            label="v"
-          />
-          <Vector
-            components={ePrime}
+            x={ePrime[0]}
+            y={ePrime[1]}
+            z={ePrime[2]}
             color={Color.EPrime}
             label="E′"
             showComponentVectors={showComponentVectors}
           />
           <Vector
-            components={bPrime}
+            x={bPrime[0]}
+            y={bPrime[1]}
+            z={bPrime[2]}
             color={Color.BPrime}
             label="B′"
             showComponentVectors={showComponentVectors}
           />
-          {showPoynting && (
-            <>
-              <Vector
-                components={poynting}
-                color={Color.S}
-                label="S"
-                showComponentVectors={showComponentVectors}
-              />
-              <Vector
-                components={poyntingPrime}
-                color={Color.SPrime}
-                label="S′"
-                showComponentVectors={showComponentVectors}
-              />
-            </>
-          )}
+          <Vector
+            x={poynting[0]}
+            y={poynting[1]}
+            z={poynting[2]}
+            color={Color.S}
+            label="S"
+            showComponentVectors={showComponentVectors}
+            hide={!showPoynting}
+          />
+          <Vector
+            x={poyntingPrime[0]}
+            y={poyntingPrime[1]}
+            z={poyntingPrime[2]}
+            color={Color.SPrime}
+            label="S′"
+            showComponentVectors={showComponentVectors}
+            hide={!showPoynting}
+          />
         </Canvas>
         <form
           className="flex flex-wrap gap-8 pb-10 [&_input]:ml-3 [&_input]:border [&_legend]:text-xl [&_fieldset]:flex [&_fieldset]:flex-col [&_fieldset]:space-y-2 [&_[type=checkbox]]:ml-0 [&_[type=checkbox]]:mr-3 [&_button]:rounded [&_button]:bg-stone-600 [&_button]:py-2 [&_button]:px-3 [&_button]:text-white [&_button]:transition-colors hover:[&_button]:bg-stone-600/[.85] focus-visible:[&_button]:bg-stone-600/[.85]"
