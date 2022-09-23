d7ab9f9c8d7772650fa62b7a9cf09c7000c06751
Michael Warner <michaelwarner@Michaels-MacBook-Pro.local>
2022-09-23 18:10:18 -0400

    small tweak to Options mechanism

--- components/Options.tsx
+++ components/Options.tsx
@@ -45,187 +45,192 @@ const Options = memo(({ cameraRef }: Props) => {
   );
 
   return (
-    <fieldset className="w-full">
-      <legend>Options</legend>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={showComponentVectors}
-            disabled={hideBoostedQuantities}
-            onChange={(e) => setShowComponentVectors(e.target.checked)}
-          />
-          Show component-vectors parallel and perpendicular to boost-velocity.
-        </label>
-      </div>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={showPoynting}
-            onChange={(e) => setShowPoynting(e.target.checked)}
-          />
-          Show the Poynting vector (S = E x B).
-        </label>
-      </div>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={
-              showParticleVelocity ||
-              showLorentzForce ||
-              showParticleAcceleration
-            }
-            disabled={showLorentzForce || showParticleAcceleration}
-            onChange={(e) => setShowParticleVelocity(e.target.checked)}
-          />
-          Show particle velocity (u).
-        </label>
-      </div>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={showLorentzForce || showParticleAcceleration}
-            disabled={showParticleAcceleration}
-            onChange={(e) => setShowLorentzForce(e.target.checked)}
-          />
-          Show the Lorentz force (F = q(E + u x B)) acting on the particle.
-          (Will show the particle velocity, too.)
-        </label>
-      </div>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={showParticleAcceleration}
-            onChange={(e) => setShowParticleAcceleration(e.target.checked)}
-          />
-          Show the particle's acceleration (a = (F - (F • v)v) / (γm), γ = 1/√(1
-          - v²)) resulting from the Lorentz force. (Will show the particle
-          velocity and the Lorentz force, too.)
-        </label>
-      </div>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={hideBoostedQuantities}
-            onChange={(e) => setHideBoostedQuantities(e.target.checked)}
-          />
-          Hide the boost-velocity and the boosted quantities.
-        </label>
-      </div>
-      <div>
-        <button
-          type="button"
-          onClick={() => {
-            if (cameraRef.current) {
-              cameraRef.current.reset();
-            }
-          }}
-        >
-          Reset Camera
-        </button>
-      </div>
-
-      <details className="space-y-2">
-        <summary className="mt-2 w-max cursor-pointer">
-          Some interesting preset field configurations
-        </summary>
-        <div className="flex flex-wrap gap-2">
-          <div>
-            <button
-              type="button"
-              onClick={() => {
-                setEField([0, 1, 0]);
-                setBField([0, 0, 1]);
-              }}
-            >
-              Light-wave toward x
-            </button>
-          </div>
-          <div>
-            <button
-              type="button"
-              onClick={() => {
-                setEField([-1, 0, 0]);
-                setBField([0, -1, 0]);
-              }}
-            >
-              Light-wave toward z
-            </button>
-          </div>
-          <div>
-            <button
-              type="button"
-              onClick={() => {
-                setEField([0, 0, 1]);
-                setBField([0, 0, 1]);
-              }}
-            >
-              Parallel fields on z
-            </button>
-          </div>
-          <div>
-            <button
-              type="button"
-              onClick={() => {
-                setEField([1, 1, 1]);
-                setBField([1, 1, 1]);
-              }}
-            >
-              Parallel fields, tilted
-            </button>
-          </div>
-          <div>
-            <button
-              type="button"
-              onClick={() => {
-                setEField([0, 0, 1]);
-                setBField([0, 0, -1]);
-              }}
-            >
-              Anti-parallel fields on z
-            </button>
-          </div>
-          <div>
-            <button
-              type="button"
-              onClick={() => {
-                setEField([1, 1, 1]);
-                setBField([-1, -1, -1]);
-              }}
-            >
-              Anti-parallel fields, tilted
-            </button>
-          </div>
-          <div>
-            <button
-              type="button"
-              onClick={() => {
-                setEField([0, 0, 0]);
-                setBField([-1, 1, -1]);
-              }}
-            >
-              No electric
-            </button>
-          </div>
-          <div>
-            <button
-              type="button"
-              onClick={() => {
-                setBField([0, 0, 0]);
-                setEField([-1, 1, -1]);
-              }}
-            >
-              No magnetic
-            </button>
-          </div>
+    <details className="w-full">
+      <summary className="mt-2 w-max cursor-pointer">Options</summary>
+      <fieldset className="ml-4">
+        <legend className="sr-only">Options</legend>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={showComponentVectors && !hideBoostedQuantities}
+              disabled={hideBoostedQuantities}
+              onChange={(e) => setShowComponentVectors(e.target.checked)}
+            />
+            Show component-vectors parallel and perpendicular to boost-velocity.
+          </label>
         </div>
-      </details>
-    </fieldset>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={showPoynting}
+              onChange={(e) => setShowPoynting(e.target.checked)}
+            />
+            Show the Poynting vector (S = E x B).
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={
+                showParticleVelocity ||
+                showLorentzForce ||
+                showParticleAcceleration
+              }
+              disabled={showLorentzForce || showParticleAcceleration}
+              onChange={(e) => setShowParticleVelocity(e.target.checked)}
+            />
+            Show particle velocity (u).
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={showLorentzForce || showParticleAcceleration}
+              disabled={showParticleAcceleration}
+              onChange={(e) => setShowLorentzForce(e.target.checked)}
+            />
+            Show the Lorentz force (F = q(E + u x B)) acting on the particle.
+            (Will show the particle velocity, too.)
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={showParticleAcceleration}
+              onChange={(e) => setShowParticleAcceleration(e.target.checked)}
+            />
+            Show the particle's acceleration (a = (F - (F • v)v) / (γm), γ =
+            1/√(1 - v²)) resulting from the Lorentz force. (Will show the
+            particle velocity and the Lorentz force, too.)
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={hideBoostedQuantities}
+              onChange={(e) => setHideBoostedQuantities(e.target.checked)}
+            />
+            Hide the boost-velocity and the boosted quantities. (Will also hide
+            the component-vectors parallel and perpendicular to the
+            boost-velocity.)
+          </label>
+        </div>
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              if (cameraRef.current) {
+                cameraRef.current.reset();
+              }
+            }}
+          >
+            Reset Camera
+          </button>
+        </div>
+
+        <details className="space-y-2">
+          <summary className="mt-2 w-max cursor-pointer">
+            Some interesting preset field configurations
+          </summary>
+          <div className="flex flex-wrap gap-2">
+            <div>
+              <button
+                type="button"
+                onClick={() => {
+                  setEField([0, 1, 0]);
+                  setBField([0, 0, 1]);
+                }}
+              >
+                Light-wave toward x
+              </button>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={() => {
+                  setEField([-1, 0, 0]);
+                  setBField([0, -1, 0]);
+                }}
+              >
+                Light-wave toward z
+              </button>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={() => {
+                  setEField([0, 0, 1]);
+                  setBField([0, 0, 1]);
+                }}
+              >
+                Parallel fields on z
+              </button>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={() => {
+                  setEField([1, 1, 1]);
+                  setBField([1, 1, 1]);
+                }}
+              >
+                Parallel fields, tilted
+              </button>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={() => {
+                  setEField([0, 0, 1]);
+                  setBField([0, 0, -1]);
+                }}
+              >
+                Anti-parallel fields on z
+              </button>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={() => {
+                  setEField([1, 1, 1]);
+                  setBField([-1, -1, -1]);
+                }}
+              >
+                Anti-parallel fields, tilted
+              </button>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={() => {
+                  setEField([0, 0, 0]);
+                  setBField([-1, 1, -1]);
+                }}
+              >
+                No electric
+              </button>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={() => {
+                  setBField([0, 0, 0]);
+                  setEField([-1, 1, -1]);
+                }}
+              >
+                No magnetic
+              </button>
+            </div>
+          </div>
+        </details>
+      </fieldset>
+    </details>
   );
 });
 
