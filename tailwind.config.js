fe02ce080f598372ab10707a261b9d6d843e65dc
Michael Warner <michaelwarner@Michaels-MacBook-Pro.local>
2022-09-26 13:17:34 -0400

    use em instead of px for breakpoints for better text-zoom accessibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
+/** @param {number} num */
+const round = (num) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, '$1')
+    .replace(/\.0$/, '');
+
+/** @param {number} px */
+const em = (px, base = 16) => `${round(px / base)}em`;
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,6 +16,15 @@ module.exports = {
     './helpers/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      /* Tailwind defaults, but in `em` (see https://danburzo.ro/media-query-units/) */
+      sm: em(640),
+      md: em(768),
+      lg: em(1024),
+      xl: em(1280),
+      // I prefer to stop at `xl`
+      // '2xl': em(1536)
+    },
     extend: {
       container: {
         center: true,
