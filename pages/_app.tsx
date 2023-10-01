c92e25cefa8433416b10110025b856887e058d52
Michael Warner <michaelwarner@Michaels-MacBook-Pro-2.local>
2023-10-01 13:05:09 -0400

    undo previous commit bc results in the 'hidden' equation being focusable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { MathJax, MathJaxContext } from 'better-react-mathjax';
+import { MathJaxContext } from 'better-react-mathjax';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MathJaxContext src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js">
       <Component {...pageProps} />
-      {/*
-        Just here to force the app's needed MathJax fonts to download on page-load
-        (otherwise they won't download until the user opens a `<details>` element
-        that uses them, at which point the user will see a flash of untyped math
-        while the fonts download).
-      */}
-      <div aria-hidden className="sr-only">
-        <MathJax>{'\\( = \\left (  \\, \\vec u  \\right ) \\)'}</MathJax>
-      </div>
     </MathJaxContext>
   );
 }
