e795c483f57fd0a31b0fc5976d0d3bed8c606db4
Michael Warner <michaelwarner@Michaels-MacBook-Pro-2.local>
2024-02-03 12:41:48 -0500

    tweak basic ThemeSwitch (NOT done)

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -2,24 +2,29 @@ import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 
 const ThemeSwitch = () => {
-  const [hydrated, setHydrated] = useState(false);
+  const [isHydrated, setIsHydrated] = useState(false);
   const { theme, setTheme } = useTheme();
 
   useEffect(() => {
-    setHydrated(true);
+    setIsHydrated(true);
   }, []);
 
-  if (!hydrated) {
-    return null;
-  }
-
   return (
-    <div>
-      <select value={theme} onChange={(e) => setTheme(e.target.value)}>
-        <option value="system">System</option>
-        <option value="dark">Dark</option>
-        <option value="light">Light</option>
-      </select>
+    <div
+      style={{ '--opacity': isHydrated ? '100%' : '0%' } as React.CSSProperties}
+      className="opacity-[--opacity] transition-opacity"
+    >
+      {isHydrated ? (
+        <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+          <option value="system">System</option>
+          <option value="dark">Dark</option>
+          <option value="light">Light</option>
+        </select>
+      ) : (
+        <select>
+          <option value=""></option>
+        </select>
+      )}
     </div>
   );
 };
