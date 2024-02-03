470982b5789ec9f79b3e7f30f3d9afe7ade7c9ef
Michael Warner <michaelwarner@Michaels-MacBook-Pro-2.local>
2024-02-03 12:54:32 -0500

    another ThemeSwitch tweak (still need positioning and real styling)

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -9,19 +9,31 @@ const ThemeSwitch = () => {
     setIsHydrated(true);
   }, []);
 
+  const selectStyles = /* tw */ 'inline-block';
+
   return (
     <div
       style={{ '--opacity': isHydrated ? '100%' : '0%' } as React.CSSProperties}
       className="opacity-[--opacity] transition-opacity"
     >
       {isHydrated ? (
-        <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+        <select
+          value={theme}
+          onChange={(e) => setTheme(e.target.value)}
+          className={selectStyles}
+        >
           <option value="system">System</option>
           <option value="dark">Dark</option>
           <option value="light">Light</option>
         </select>
       ) : (
-        <select>
+        <select
+          aria-hidden
+          hidden
+          tabIndex={-1}
+          {...{ inert: '' }}
+          className={`${selectStyles} select-none pointer-events-none`}
+        >
           <option value=""></option>
         </select>
       )}
